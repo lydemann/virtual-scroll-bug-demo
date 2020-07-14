a6e4a7447c9eb5cc9fa8caeab4b4dc68cbeea431
Christian Lüdemann <christian@christianlydemann.com>
2020-07-14 18:19:04 +0200

    chore: reproduce virtual scroll rendering issue when nested in many divs

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -7,6 +7,47 @@ import { Component } from '@angular/core';
 })
 export class Tab1Page {
 
-  constructor() {}
+  items: any[] = [];
+  lorem = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, ';
 
+  images = [
+    'bandit',
+    'batmobile',
+    'blues-brothers',
+    'bueller',
+    'delorean',
+    'eleanor',
+    'general-lee',
+    'ghostbusters',
+    'knight-rider',
+    'mirth-mobile'
+  ];
+
+  rotateImg = 0;
+
+  constructor() {
+    for (let i = 0; i < 1000; i++) {
+      this.items.push({
+        name: i + ' - ' + this.images[this.rotateImg],
+        imgSrc: this.getImgSrc(),
+        avatarSrc: this.getImgSrc(),
+        imgHeight: 10,
+        content: this.lorem.substring(0, Math.random() * (this.lorem.length - 100) + 100)
+      });
+
+      this.rotateImg++;
+      if (this.rotateImg === this.images.length) {
+        this.rotateImg = 0;
+      }
+    }
+  }
+
+  getImgSrc() {
+    const src = 'https://dummyimage.com/600x400/${Math.round( Math.random() * 99999)}/fff.png';
+    this.rotateImg++;
+    if (this.rotateImg === this.images.length) {
+      this.rotateImg = 0;
+    }
+    return src;
+  }
 }
